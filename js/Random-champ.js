9072649a27a1524c795076fdec920a04882ee6a9
RemiMeere <73473703+RemiMeere@users.noreply.github.com>
2020-12-08 10:15:57 +0100

    Add files via upload

--- js/Random-champ.js
+++ js/Random-champ.js
@@ -6,14 +6,18 @@ var champ = {
 function initialiserPage(){
 	document.getElementsByTagName("SECTION")[2].innerHTML = "<table> <thead><th>Nom</th><th>random champ</th></thead><tbody id='hero'></tbody></table>"
 }
+/** recupaire l'objet voulu par l'utilisateur et sélection un "champion" aléatoirement*/
 function role(formulaire){
 	console.log(champ)
-	let temp=document.getElementById("nom").value;
+	let temp=formulaire.nom.value;
 	let str = champ[temp].split(' ');
 	Math.floor(Math.random() * 3);
 	document.getElementById("hero").innerHTML = str[Math.floor(Math.random() * 3)];
+	return false;
 }
+/** permet de créé un nouveaux type de "champion" et y metre des héros  */
 function newrole(formulaire){
-	champ[document.getElementById("new").value] =document.getElementById("newchamp").value;
+	champ[formulaire.new.value] =formulaire.newchamp.value;
 	console.log(champ)
+	return false;
 }
